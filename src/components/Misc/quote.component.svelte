8965288c6ba67061a076351f357aad6e87e1524d
Saptarshi Basu <123saptarshi.basu@gmail.com>
2021-12-24 17:28:00 +0530

    fix

--- src/components/Misc/quote.component.svelte
+++ src/components/Misc/quote.component.svelte
@@ -1,4 +1,4 @@
-<div class="flex space-x-4 text-sm hover:bg-gray-100 cursor-pointer duration-100">
+<div class="flex text-left space-x-4 text-sm hover:bg-gray-100 cursor-pointer duration-100">
 	<div class="w-1 h-auto bg-brand-black" />
 	<div class="py-1.5"><slot /></div>
 </div>
