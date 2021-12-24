7f747b223a4af2e5665a164c58e36db94842b1c0
Saptarshi Basu <123saptarshi.basu@gmail.com>
2021-12-24 17:17:07 +0530

    fix'

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,7 @@
 	import Navigation from '../components/Navigation/navigation.component.svelte';
 </script>
 
-<main class="flex flex-col items-center px-4 lg:px-0 justify-center w-full text-brand-black">
+<main class="flex flex-col items-center px-6 lg:px-0 justify-center w-full text-brand-black">
 	<Navigation />
 
 	<div class="max-w-[700px] w-full mt-28 md:mt-32">
